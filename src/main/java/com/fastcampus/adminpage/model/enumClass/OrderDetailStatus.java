b7cb2b58ce1dd557f10c7f23bbc036ec8dbe378a
BrianDYKim <18k7102dy@naver.com>
2021-11-02 16:57:37 +0900

    2021-11-02 commit

--- src/main/java/com/fastcampus/adminpage/model/enumClass/OrderDetailStatus.java
+++ src/main/java/com/fastcampus/adminpage/model/enumClass/OrderDetailStatus.java
@@ -9,7 +9,9 @@ public enum OrderDetailStatus {
     REGISTERED(0, "등록", "주문 등록"),
     UNREGISTERED(1, "해지", "주문 해지"),
     WAITING(2, "검수", "주문 검수중"),
-    COMPLETE(3, "완료", "배송 완료");
+    COMPLETE(3, "완료", "배송 완료"),
+    ORDERING(4, "주문중", "주문 진행중"),
+    CONFIRM(5, "주문 수락", "주문이 정상 수락되었습니다");
 
     private Integer id;
     private String title;
--- src/main/java/com/fastcampus/adminpage/model/enumClass/orderGroup/OrderGroupStatus.java
+++ src/main/java/com/fastcampus/adminpage/model/enumClass/orderGroup/OrderGroupStatus.java
@@ -10,7 +10,9 @@ public enum OrderGroupStatus {
     REGISTERED(0, "등록", "주문 묶음 등록"),
     UNREGISTERED(1, "해지", "주문 묶음 해지"),
     WAITING(2, "검수", "주문 묶음 검수중"),
-    COMPLETE(3, "배송완료", "주문 묶음 배송완료");
+    COMPLETE(3, "배송완료", "주문 묶음 배송완료"),
+    ORDERING(4, "주문중", "주문 진행중"),
+    CONFIRM(5, "주문 수락", "주문이 정상 수락되었습니다");
 
     private Integer id;
     private String title;
